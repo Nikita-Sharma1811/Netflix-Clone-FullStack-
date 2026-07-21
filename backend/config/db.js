eed3a0025c916ea117daf60bee001e63097903ee
Nikita Sharma <nikitash1811@gmail.com>
2026-07-21 11:08:44 +0530

    coomit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/netflixClone");
+    await mongoose.connect("mongodb+srv://nikitash1811_db_user:nikita@2006@cluster0.xbsgbtc.mongodb.net/");
 
     console.log("✅ MongoDB Connected");
   } catch (error) {
