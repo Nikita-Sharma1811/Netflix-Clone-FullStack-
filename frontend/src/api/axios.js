f63abd82826462f4216cbae8b34c7fa29b72345a
Nikita Sharma <nikitash1811@gmail.com>
2026-07-21 11:47:42 +0530

    coomit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://netflix-clone-fullstack-fi2n.onrender.com",
+    baseURL: "http://localhost:5000/api",
 });
 
 export default API;
